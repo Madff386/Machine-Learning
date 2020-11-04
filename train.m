b5893acae5a588d659c6ec03409fe7ec0e275789
Madff386 <69940651+Madff386@users.noreply.github.com>
2020-11-05 07:24:40 +1100

    Update train.m

--- train.m
+++ train.m
@@ -1,3 +1,11 @@
-function params = train(X, y, hidden_layer_size, lambda)
+function params = train(X, y, hidden_layer_size, lambda, max_iter)
 
 
+m = size(X, 1);
+n = size(X, 2);
+ouput_layer_size = size(y, 2);
+
+
+
+options = optimset("MaxIter", max_iter);
+[nn_params, cost] = fmincg(nn_cost_function, initial_params, options);
