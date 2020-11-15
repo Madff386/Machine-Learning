0f4458fd55a00f4c76848c16d9d682648c6f7f47
Madff386 <69940651+Madff386@users.noreply.github.com>
2020-11-16 09:00:15 +1100

    Update train.m

--- train.m
+++ train.m
@@ -1,4 +1,4 @@
-function params = train(X, y, hidden_layer_size, lambda, max_iter)
+function nn_params = train(X, y, hidden_layer_size, lambda, max_iter)
 
 
 m = size(X, 1);
@@ -7,5 +7,13 @@ ouput_layer_size = size(y, 2);
 
 
 
+
+initial_theta_1 = randInitializeWeights(n, hidden_layer_size);
+initial_theta_2 = randInitializeWeights(hidden_layer_size, ouput_layer_size);
+initial_params = [initial_theta_1(:) ; initial_theta_2(:)];
+
+
+
 options = optimset("MaxIter", max_iter);
-[nn_params, cost] = fmincg(nn_cost_function, initial_params, options);
+costFunction = @(p) nnCostFunction(p, n, hidden_layer_size, num_labels, X ,y, lambda);
+[nn_params, ~ ] = fmincg(costFunction, initial_params, options);
